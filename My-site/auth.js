2c310f75a290cd4818ac6cadca3a339ac58a8034
Project486 <169157731+Project486@users.noreply.github.com>
2025-06-23 16:22:04 +0300

    Delete My-site directory

--- My-site/auth.js
+++
@@ -1,8 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-const code = urlParams.get("code");
-
-if (code === "1234") {
-  document.getElementById("protected-content").style.display = "block";
-} else {
-  document.body.innerHTML = "<h1>אין גישה לעמוד הזה</h1>";
-}
